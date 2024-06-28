dc6923ea1b14430a7c1f06467322f2d1466510c6
Emir Tarlabelen <emirtarlabelen021@gmail.com>
2024-06-29 01:02:53 +0300

    Front end changed

--- src/layout/Dashboard/Drawer/DrawerContent/Navigation/index.jsx
+++ src/layout/Dashboard/Drawer/DrawerContent/Navigation/index.jsx
@@ -1,26 +1,59 @@
-// material-ui
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import React from 'react';
+import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import { Link } from 'react-router-dom';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import TextFormatIcon from '@mui/icons-material/TextFormat';
+import FilterDramaIcon from '@mui/icons-material/FilterDrama'; // Uygun ikon
+import EventIcon from '@mui/icons-material/Event';
+import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
+import SampleIcon from '@mui/icons-material/InsertDriveFile';
 
-// project import
-import NavGroup from './NavGroup';
-import menuItem from 'menu-items';
+const Navigation = () => (
+  <List>
+    <ListItem button component={Link} to="/dashboard/default">
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="Dashboard" />
+    </ListItem>
+    <ListItem button component={Link} to="/color">
+      <ListItemIcon>
+        <CalendarTodayIcon />
+      </ListItemIcon>
+      <ListItemText primary="Ders Programı" />
+    </ListItem>
+    <ListItem button component={Link} to="/typography">
+      <ListItemIcon>
+        <TextFormatIcon />
+      </ListItemIcon>
+      <ListItemText primary="Okul Duyuruları" />
+    </ListItem>
+    <ListItem button component={Link} to="/shadow">
+      <ListItemIcon>
+        <FilterDramaIcon />
+      </ListItemIcon>
+      <ListItemText primary="Shadow" />
+    </ListItem>
+    <ListItem button component={Link} to="/events">
+      <ListItemIcon>
+        <EventIcon />
+      </ListItemIcon>
+      <ListItemText primary="Okul Etkinlikleri" />
+    </ListItem>
+    <ListItem button component={Link} to="/buy-ticket">
+      <ListItemIcon>
+        <ConfirmationNumberIcon />
+      </ListItemIcon>
+      <ListItemText primary="Bilet Satın Al" />
+    </ListItem>
+    <ListItem button component={Link} to="/sample-page">
+      <ListItemIcon>
+        <SampleIcon />
+      </ListItemIcon>
+      <ListItemText primary="Bilet Listesi" />
+    </ListItem>
+  </List>
+);
 
-// ==============================|| DRAWER CONTENT - NAVIGATION ||============================== //
-
-export default function Navigation() {
-  const navGroups = menuItem.items.map((item) => {
-    switch (item.type) {
-      case 'group':
-        return <NavGroup key={item.id} item={item} />;
-      default:
-        return (
-          <Typography key={item.id} variant="h6" color="error" align="center">
-            Fix - Navigation Group
-          </Typography>
-        );
-    }
-  });
-
-  return <Box sx={{ pt: 2 }}>{navGroups}</Box>;
-}
+export default Navigation;
