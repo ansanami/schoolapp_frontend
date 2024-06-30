17d254ebd41cd8f2249318860d6595361f055d9e
Recep Kırtay <recepkirtay23@hotmail.com>
2024-06-30 18:21:07 +0300

    yenileme sorunu çözüldü

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,4 +1,3 @@
-// https://github.com/vitejs/vite/discussions/3448
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -8,8 +7,7 @@ import jsconfigPaths from 'vite-jsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), jsconfigPaths()],
-  // https://github.com/jpuri/react-draft-wysiwyg/issues/1317
-  base: '/free/login', // accessing env variable is not possible here. So hard coding this.
+  base: '/',
   define: {
     global: 'window'
   },
@@ -26,15 +24,11 @@ export default defineConfig({
     ]
   },
   server: {
-    // this ensures that the browser opens upon server start
     open: true,
-    // this sets a default port to 3000
     port: 3000
   },
   preview: {
-    // this ensures that the browser opens upon preview start
     open: true,
-    // this sets a default port to 3000
     port: 3000
   }
 });
