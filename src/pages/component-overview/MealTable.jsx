15320089ce06e01b8436d8b0aed8516238f15eda
EmirC21 <112898811+EmirC21@users.noreply.github.com>
2024-07-01 21:25:09 +0300

    Meal_TAble_entegration
    updated

--- src/pages/component-overview/MealTable.jsx
+++ src/pages/component-overview/MealTable.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import { useTheme } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
@@ -13,14 +14,6 @@ import Paper from '@mui/material/Paper';
 import MainCard from 'components/MainCard';
 import ComponentWrapper from './ComponentWrapper';
 import ComponentSkeleton from './ComponentSkeleton';
-
-const meals = {
-  Pazartesi: ['Köfte', 'Pilav', 'Domates Çorbası', 'Cacık'],
-  Salı: ['Tavuk', 'Makarna', 'Mercimek Çorbası', 'Salata'],
-  Çarşamba: ['Balık', 'Bulgur', 'Tarhana Çorbası', 'Turşu'],
-  Perşembe: ['Et Sote', 'Patates Püresi', 'Yoğurt Çorbası', 'Ayran'],
-  Cuma: ['Mantı', 'Kısır', 'Sebze Çorbası', 'Komposto']
-};
 
 function MealTable({ day, meals }) {
   const theme = useTheme();
@@ -54,16 +47,40 @@ MealTable.propTypes = {
 };
 
 export default function CafeteriaMenu() {
+  const [mealsData, setMealsData] = useState({});
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/yemekhane', {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    })
+    .then(response => {
+      const formattedData = response.data.reduce((acc, item) => {
+        acc[item.gun] = [item.yemek1, item.yemek2, item.yemek3, item.yemek4];
+        return acc;
+      }, {});
+      setMealsData(formattedData);
+    })
+    .catch(error => {
+      console.error('There was an error fetching the meals data!', error);
+    });
+  }, []);
+
+  const daysOrder = ['Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma'];
+
   return (
     <ComponentSkeleton>
       <ComponentWrapper>
         <Grid container spacing={3}>
-          {Object.keys(meals).map((day) => (
-            <Grid item xs={12} sm={6} md={4} lg={3} key={day}>
-              <MainCard title={day} shadow="2">
-                <MealTable day={day} meals={meals[day]} />
-              </MainCard>
-            </Grid>
+          {daysOrder.map((day) => (
+            mealsData[day] && (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={day}>
+                <MainCard title={day} shadow="2">
+                  <MealTable day={day} meals={mealsData[day]} />
+                </MainCard>
+              </Grid>
+            )
           ))}
         </Grid>
       </ComponentWrapper>
