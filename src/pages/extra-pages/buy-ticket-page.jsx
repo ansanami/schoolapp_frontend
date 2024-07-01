9121dbbc1f8e0ed1572374f420086ea038a21db9
Recep Kırtay <recepkirtay23@hotmail.com>
2024-07-01 20:16:56 +0300

    koltuk düzeni

--- src/pages/extra-pages/buy-ticket-page.jsx
+++ src/pages/extra-pages/buy-ticket-page.jsx
@@ -11,7 +11,7 @@ export default function BuyTicketPage() {
   const location = useLocation();
   const params = new URLSearchParams(location.search);
   const eventTitle = params.get('event');
-  const sessionId = 1; // Bu parametreyi dinamik olarak almak gerekebilir.
+  const sessionId = 1;
   const [seats, setSeats] = useState([]);
   const [selectedSeat, setSelectedSeat] = useState(null);
 
@@ -34,46 +34,46 @@ export default function BuyTicketPage() {
 
   const renderSeats = () => {
     const rows = [...new Set(seats.map((seat) => seat.actSeat.line))];
+    const seatsPerRow = seats.filter((seat) => seat.actSeat.line === rows[0]).length;
 
     return rows.map((row) => (
       <div key={row}>
         <Typography variant="h6" align="left">{`Sıra ${row}`}</Typography>
         <Grid container spacing={2} justifyContent="center" alignItems="center" style={{ marginBottom: '20px' }}>
-          {seats
-            .filter((seat) => seat.actSeat.line === row)
-            .map((seat) => {
-              const isReserved = seat.status !== 'AVAILABLE';
+          {Array.from({ length: seatsPerRow }).map((_, index) => {
+            const seat = seats.find(seat => seat.actSeat.line === row && seat.actSeat.no === index + 1);
+            const isReserved = seat?.status !== 'AVAILABLE';
 
-              return (
-                <Grid item key={seat.actSeat.no}>
-                  <Button
-                    variant="outlined"
-                    onClick={() => handleSeatSelection(seat)}
-                    disabled={isReserved}
-                    style={{
-                      backgroundColor: isReserved ? 'gray' : selectedSeat?.actSeat.id === seat.actSeat.id ? 'yellow' : 'lightgreen',
-                      minWidth: '60px',
-                      minHeight: '60px',
-                      display: 'flex',
-                      flexDirection: 'column',
-                      alignItems: 'center'
-                    }}
-                  >
-                    <ChairIcon />
-                    <Typography variant="caption">{`${row}${seat.actSeat.no}`}</Typography>
-                  </Button>
-                </Grid>
-              );
-            })}
+            return (
+              <Grid item key={index}>
+                <Button
+                  variant="outlined"
+                  onClick={() => seat && handleSeatSelection(seat)}
+                  disabled={!seat || isReserved}
+                  style={{
+                    backgroundColor: !seat ? 'transparent' : isReserved ? 'gray' : selectedSeat?.actSeat.id === seat.actSeat.id ? 'yellow' : 'lightgreen',
+                    minWidth: '60px',
+                    minHeight: '60px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                  }}
+                >
+                  <ChairIcon />
+                  {seat && <Typography variant="caption">{`${row}${seat.actSeat.no}`}</Typography>}
+                </Button>
+              </Grid>
+            );
+          })}
         </Grid>
       </div>
     ));
   };
 
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div style={{ textAlign: 'center', maxWidth: '100%' }}>
       <Typography variant="h4">Bilet Satın Al - {eventTitle}</Typography>
-      <Paper elevation={3} style={{ padding: '30px', margin: '20px auto', maxWidth: '1200px' }}>
+      <Paper elevation={3} style={{ padding: '30px', margin: '20px auto', maxWidth: '95%' }}>
         {renderSeats()}
       </Paper>
       {selectedSeat && (
